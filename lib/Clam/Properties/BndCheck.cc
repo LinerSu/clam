45692be964f20a21dc56a7f050f0dfb03482eff3
yusen <y256su@uwaterloo.ca>
2021-08-30 17:34:26 +0000

    refactor(lib): refactor bnd check methods

--- lib/Clam/Properties/BndCheck.cc
+++ lib/Clam/Properties/BndCheck.cc
@@ -26,7 +26,8 @@ class EmitBndChecksImpl {
   const DataLayout * m_dl;
   using ref_bnd_map_t = llvm::DenseMap<llvm::Value *, std::pair<var_t, var_t>>;
   // map llvm pointer to pair of object size and offset
-  std::unique_ptr<ref_bnd_map_t> m_ref_bnd_map;
+  ref_bnd_map_t m_ref_bnd_map;
+  const unsigned m_width = 64; // Fixed width for vars of size and offset
 
   // Private function here:
   // Get datalayout by given an instruction
@@ -45,9 +46,9 @@ class EmitBndChecksImpl {
   // where offset indicates the offset of current pointer
   // the size means the total size of allcation
   // sizeof(value) gets the bit-width of the element
-  void addBndCheckAssertion(const var_t &reg, const var_t &size,
-                            const var_t &offset, basic_block_t &bb,
-                            Instruction &I, unsigned size_of) {
+  void addBndCheckAssertion(Instruction &I, const var_t &reg, basic_block_t &bb,
+                            bool is_load_ref) {
+    std::string log_out = is_load_ref ? "load" : "store";
     if (m_params.check_only_typed_regions &&
         reg.get_type().is_unknown_region()) {
       return;
@@ -58,17 +59,38 @@ class EmitBndChecksImpl {
     // about the HeapAbstraction Region such as whether
     // isSequence(), IsCyclic(), etc.
 
-    bb.assertion(offset >= number_t(0), getDebugLoc(&I, m_assertionId++));
-    bb.assertion(size >= offset + size_of, getDebugLoc(&I, m_assertionId++));
-    m_addedChecks++;
-    return;
+    llvm::Value *vptr;
+    if (LoadInst *loadI = dyn_cast<LoadInst>(&I))
+      vptr = loadI->getPointerOperand();
+    else if (StoreInst *storeI = dyn_cast<StoreInst>(&I))
+      vptr = storeI->getPointerOperand();
+    else
+      CLAM_ERROR(
+          "Expect adding bound check assertions at load/store instrucitons");
+    auto it = m_ref_bnd_map.find(vptr);
+    if (it == m_ref_bnd_map.end()) {
+      CLAM_WARNING("Could not find " << log_out << " ptr from map " << *vptr);
+    } else {
+      CRAB_LOG("cfg-bnd-check",
+               llvm::errs()
+                   << log_out << ": check size and offset are in bounds: " << I
+                   << "\n");
+      var_t size = it->second.first;
+      var_t offset = it->second.second;
+      Type *elemTy = vptr->getType()->getPointerElementType();
+      const llvm::DataLayout *dl = getInsDatalayout(I);
+      unsigned size_of = dl->getTypeSizeInBits(elemTy) / 8;
+      bb.assertion(offset >= number_t(0), getDebugLoc(&I, m_assertionId++));
+      bb.assertion(size >= offset + size_of, getDebugLoc(&I, m_assertionId++));
+      m_addedChecks++;
+    }
   }
 
 public:
   EmitBndChecksImpl(const CrabBuilderParams &params, crabLitFactory &lfac,
                     uint32_t &assertionId)
       : m_params(params), m_lfac(lfac), m_assertionId(assertionId),
-        m_addedChecks(0), m_dl(nullptr), m_ref_bnd_map(std::make_unique<ref_bnd_map_t>()) {}
+        m_addedChecks(0), m_dl(nullptr) {}
 
   ~EmitBndChecksImpl() {
     llvm::errs() << "-- Inserted " << m_addedChecks;
@@ -76,57 +98,27 @@ public:
   }
 
   void visitBeforeStore(llvm::StoreInst &I, CrabStoreRefOps &s) {
-    Value *key = I.getPointerOperand();
-    basic_block_t &bb = s.getBasicBlock();
-    auto it = m_ref_bnd_map->find(key);
-    if (it == m_ref_bnd_map->end()) {
-      CLAM_WARNING("Could not find store ptr from map " << *key);
-    } else {
-      CRAB_LOG("cfg-bnd-check",
-               llvm::errs()
-                   << "store: check size and offset are in bounds: " << I
-                   << "\n");
-      var_t ptr_obj_sz = it->second.first;
-      var_t ptr_offset = it->second.second;
-      Type *elemTy = key->getType()->getPointerElementType();
-      const llvm::DataLayout *dl = getInsDatalayout(I);
-      unsigned size_of = dl->getTypeSizeInBits(elemTy) / 8;
-      addBndCheckAssertion(s.getRegion(), ptr_obj_sz, ptr_offset, bb, I, size_of);
-    }
+    addBndCheckAssertion(I, s.getRegion(), s.getBasicBlock(), false);
   }
 
   void visitBeforeLoad(llvm::LoadInst &I, CrabLoadRefOps &s) {
-    Value *key = I.getPointerOperand();
-    basic_block_t &bb = s.getBasicBlock();
-    auto it = m_ref_bnd_map->find(key);
-    if (it == m_ref_bnd_map->end()) {
-      CLAM_WARNING("Could not find load ptr from map " << *key);
-    } else {
-      CRAB_LOG("cfg-bnd-check",
-               llvm::errs()
-                   << "load: check size and offset are in bounds: " << I
-                   << "\n");
-      var_t ptr_obj_sz = it->second.first;
-      var_t ptr_offset = it->second.second;
-      Type *elemTy = key->getType()->getPointerElementType();
-      const llvm::DataLayout *dl = getInsDatalayout(I);
-      unsigned size_of = dl->getTypeSizeInBits(elemTy) / 8;
-      addBndCheckAssertion(s.getRegion(), ptr_obj_sz, ptr_offset, bb, I, size_of);
-      crab_lit_ref_t lhs = m_lfac.getLit(I);
-      if (lhs->getVar().get_type().is_reference()) {
-        var_t ref_obj_sz =
-            m_lfac.mkIntVar(ptr_obj_sz.get_type().get_integer_bitwidth());
-        // obtain an overappriximation of deref
-        // using havoc to initialize size and offset
-        bb.havoc(ref_obj_sz, "approx size when loading a ptr of ptr");
-        var_t ref_offset =
-            m_lfac.mkIntVar(ptr_offset.get_type().get_integer_bitwidth());
-        bb.havoc(ref_offset, "approx offset when loading a ptr of ptr");
+    addBndCheckAssertion(I, s.getRegion(), s.getBasicBlock(), true);
+  }
 
-        // add pair to the map by llvm pointer
-        Value *ref_v = llvm::cast<llvm::Value>(&I);
-        m_ref_bnd_map->insert({ref_v, {ref_obj_sz, ref_offset}});
-      }
+  void visitAfterLoad(llvm::LoadInst &I, CrabLoadRefOps &s) {
+    crab_lit_ref_t lhs = m_lfac.getLit(I);
+    basic_block_t &bb = s.getBasicBlock();
+    if (lhs->getVar().get_type().is_reference()) {
+      var_t ref_obj_sz = m_lfac.mkIntVar(m_width);
+      // obtain an overappriximation of deref
+      // using havoc to initialize size and offset
+      bb.havoc(ref_obj_sz, "approx size when loading a ptr of ptr");
+      var_t ref_offset = m_lfac.mkIntVar(m_width);
+      bb.havoc(ref_offset, "approx offset when loading a ptr of ptr");
+
+      // add pair to the map by llvm pointer
+      Value *ref_v = llvm::cast<llvm::Value>(&I);
+      m_ref_bnd_map.insert({ref_v, {ref_obj_sz, ref_offset}});
     }
   }
 
@@ -146,15 +138,10 @@ public:
     CRAB_LOG("cfg-bnd-check",
              llvm::errs() << "alloc: add size and offset for " << I << "\n");
     llvm::Value *op;
-    unsigned sz;
-    if (isa<AllocaInst>(I)) {
-      AllocaInst *allocaI = dyn_cast<AllocaInst>(&I);
+    if (AllocaInst *allocaI = dyn_cast<AllocaInst>(&I))
       op = allocaI->getArraySize();
-      const llvm::DataLayout *dl = getInsDatalayout(*allocaI);
-      sz = dl->getTypeSizeInBits(allocaI->getAllocatedType()) / 8;
-    } else {
+    else
       op = I.getOperand(0);
-    }
     llvm::Value &operand = *op;
     crab_lit_ref_t opd = m_lfac.getLit(operand);
     if (!opd->isInt()) {
@@ -164,17 +151,23 @@ public:
     assert(opd->isInt());
     auto Iopd = std::static_pointer_cast<const crabIntLit>(opd);
     unsigned width = Iopd->getBitwidth();
-    var_t ref_obj_sz = m_lfac.mkIntVar(width);
-    if (isa<AllocaInst>(I))
-      bb.assign(ref_obj_sz, m_lfac.getExp(opd) * sz);
-    else
-      bb.assign(ref_obj_sz, m_lfac.getExp(opd));
+    var_t tmp_obj_sz = m_lfac.mkIntVar(width);
+    if (AllocaInst *allocaI = dyn_cast<AllocaInst>(&I)) {
+      const llvm::DataLayout *dl = getInsDatalayout(*allocaI);
+      unsigned sz = dl->getTypeSizeInBits(allocaI->getAllocatedType()) / 8;
+      bb.assign(tmp_obj_sz, m_lfac.getExp(opd) * sz);
+    } else {
+      bb.assign(tmp_obj_sz, m_lfac.getExp(opd));
+    }
+    var_t ref_obj_sz = m_lfac.mkIntVar(m_width);
+    if (m_width != width) // convert assigned size to fixed width 64
+      bb.zext(tmp_obj_sz, ref_obj_sz);
     // create a var_t for offset and assign it to zero
     var_t ref_offset = m_lfac.mkIntVar(width);
     bb.assign(ref_offset, number_t(0));
 
     // add pair to the map by llvm pointer
-    m_ref_bnd_map->insert({&I, {ref_obj_sz, ref_offset}});
+    m_ref_bnd_map.insert({&I, {ref_obj_sz, ref_offset}});
   }
 
   // -- get element ptr additional utilities construction
@@ -200,30 +193,25 @@ public:
     if (isa<ConstantPointerNull>(*vptr)) { // Get a constant null ptr
       Type *elemTy = vptr->getType()->getPointerElementType();
       const llvm::DataLayout *dl = getInsDatalayout(I);
-      unsigned width = dl->getTypeSizeInBits(elemTy);
-      var_t lhs_obj_sz =
-          m_lfac.mkIntVar(width);
-      var_t lhs_offset =
-          m_lfac.mkIntVar(width);
+      var_t lhs_obj_sz = m_lfac.mkIntVar(m_width);
+      var_t lhs_offset = m_lfac.mkIntVar(m_width);
       bb.assign(lhs_obj_sz, number_t(0));
       bb.assign(lhs_offset, number_t(0));
-      m_ref_bnd_map->insert({&I, {lhs_obj_sz, lhs_offset}});
+      m_ref_bnd_map.insert({&I, {lhs_obj_sz, lhs_offset}});
     }
     else {
-      auto it = m_ref_bnd_map->find(vptr);
-      if (it == m_ref_bnd_map->end()) {
+      auto it = m_ref_bnd_map.find(vptr);
+      if (it == m_ref_bnd_map.end()) {
         CLAM_WARNING("Could not find gep ptr "
                   << *vptr << " from reference map" << I);
       } else {
         var_t ptr_obj_sz = it->second.first;
         var_t ptr_offset = it->second.second;
-        var_t lhs_obj_sz =
-            m_lfac.mkIntVar(ptr_obj_sz.get_type().get_integer_bitwidth());
-        var_t lhs_offset =
-            m_lfac.mkIntVar(ptr_offset.get_type().get_integer_bitwidth());
+        var_t lhs_obj_sz = m_lfac.mkIntVar(m_width);
+        var_t lhs_offset = m_lfac.mkIntVar(m_width);
         bb.assign(lhs_obj_sz, ptr_obj_sz);
         bb.assign(lhs_offset, offset);
-        m_ref_bnd_map->insert({&I, {lhs_obj_sz, lhs_offset}});
+        m_ref_bnd_map.insert({&I, {lhs_obj_sz, lhs_offset}});
       }
     }
   }
@@ -241,44 +229,45 @@ public:
     basic_block_t &bb = s.getBasicBlock();
     Type *elemTy = vtrue.getType()->getPointerElementType();
     const llvm::DataLayout *dl = getInsDatalayout(I);
-    unsigned width = dl->getTypeSizeInBits(elemTy);
-    var_t offset_op1 = m_lfac.mkIntVar(width);
-    var_t size_op1 = m_lfac.mkIntVar(width);
-    var_t offset_op2 = m_lfac.mkIntVar(width);
-    var_t size_op2 = m_lfac.mkIntVar(width);
+    var_t cond = m_lfac.mkIntVar(m_width);
+    bb.zext(c->getVar(), cond); // extend bool into int
+    var_t offset_op1 = m_lfac.mkIntVar(m_width);
+    var_t size_op1 = m_lfac.mkIntVar(m_width);
+    var_t offset_op2 = m_lfac.mkIntVar(m_width);
+    var_t size_op2 = m_lfac.mkIntVar(m_width);
     if (m_lfac.isRefNull(op1)) {
       bb.assign(offset_op1, number_t(0));
       bb.assign(size_op1, number_t(0));
     } else {
-      auto it = m_ref_bnd_map->find(&vtrue);
-      if (it == m_ref_bnd_map->end()) {
+      auto it = m_ref_bnd_map.find(&vtrue);
+      if (it == m_ref_bnd_map.end()) {
         CLAM_WARNING("Could not find from select ptr "
                      << vtrue << " from reference map" << I);
         return;
       } else {
-        offset_op1 = it->second.first;
-        size_op1 = it->second.second;
+        size_op1 = it->second.first;
+        offset_op1 = it->second.second;
       }
     }
     if (m_lfac.isRefNull(op2)) {
       bb.assign(offset_op2, number_t(0));
       bb.assign(size_op2, number_t(0));
     } else {
-      auto it = m_ref_bnd_map->find(&vfalse);
-      if (it == m_ref_bnd_map->end()) {
+      auto it = m_ref_bnd_map.find(&vfalse);
+      if (it == m_ref_bnd_map.end()) {
         CLAM_WARNING("Could not find from select ptr "
                      << vfalse << " from reference map" << I);
         return;
       } else {
-        offset_op2 = it->second.first;
-        size_op2 = it->second.second;
+        size_op2 = it->second.first;
+        offset_op2 = it->second.second;
       }
     }
-    var_t lhs_obj_sz = m_lfac.mkIntVar(width);
-    var_t lhs_offset = m_lfac.mkIntVar(width);
-    bb.select(lhs_offset, c->getVar(), offset_op1, offset_op2);
-    bb.select(lhs_obj_sz, c->getVar(), size_op1, size_op2);
-    m_ref_bnd_map->insert({&I, {lhs_obj_sz, lhs_offset}});
+    var_t lhs_obj_sz = m_lfac.mkIntVar(m_width);
+    var_t lhs_offset = m_lfac.mkIntVar(m_width);
+    bb.select(lhs_offset, cond, offset_op1, offset_op2);
+    bb.select(lhs_obj_sz, cond, size_op1, size_op2);
+    m_ref_bnd_map.insert({&I, {lhs_obj_sz, lhs_offset}});
   }
 };
 
@@ -295,6 +284,10 @@ void EmitBndChecks::visitBeforeStore(llvm::StoreInst &I, CrabStoreRefOps &s) {
 
 void EmitBndChecks::visitBeforeLoad(llvm::LoadInst &I, CrabLoadRefOps &s) {
   m_impl->visitBeforeLoad(I, s);
+}
+
+void EmitBndChecks::visitAfterLoad(llvm::LoadInst &I, CrabLoadRefOps &s) {
+  m_impl->visitAfterLoad(I, s);
 }
 
 void EmitBndChecks::visitAfterAlloc(llvm::Instruction &I,
@@ -326,7 +319,6 @@ void EmitBndChecks::visitAfterFree(llvm::Instruction &I,
                                    CrabRemoveRefOps &s) {}
 void EmitBndChecks::visitBeforeGep(llvm::Instruction &I, CrabGepRefOps &s) {}
 void EmitBndChecks::visitAfterStore(llvm::StoreInst &I, CrabStoreRefOps &s) {}
-void EmitBndChecks::visitAfterLoad(llvm::LoadInst &I, CrabLoadRefOps &s) {}
 void EmitBndChecks::visitBeforeMemset(llvm::MemSetInst &I, CrabMemsetOps &s) {}
 void EmitBndChecks::visitAfterMemset(llvm::MemSetInst &I, CrabMemsetOps &s) {}
 void EmitBndChecks::visitBeforeMemTransfer(llvm::MemTransferInst &I,
