ae41953b3a6b5d4414f92112176e211374b1ae64
Yusen Su <y256su@uwaterloo.ca>
2025-03-19 15:05:46 -0400

    feat(tvpi): add another tvpi implementation

--- lib/Clam/crab/domains/tvpi_dbm.hh
+++ lib/Clam/crab/domains/tvpi_dbm.hh
@@ -2,13 +2,16 @@
 
 #include <crab/domains/tvpi_dbm.hpp>
 #include "crab_defs.hh"
-#include <crab/domains/fixed_tvpi_domain.hpp>
+// #include <crab/domains/fixed_tvpi_domain.hpp>
 #include "split_dbm.hh"
+#include <crab/domains/tvpi_split_dbm.hpp>
 
 namespace clam {
 using BASE(tvpi_dbm_domain_t) = tvpi_dbm_domain<BASE(split_dbm_domain_t)>;
 using tvpi_dbm_domain_t = RGN_FUN(ARRAY_FUN(BOOL_NUM(BASE(tvpi_dbm_domain_t))));
 
-using BASE(fixed_tvpi_dbm_domain_t) = fixed_tvpi_domain<BASE(split_dbm_domain_t)>;
-using fixed_tvpi_dbm_domain_t = RGN_FUN(ARRAY_FUN(BOOL_NUM(BASE(fixed_tvpi_dbm_domain_t))));
+using BASE(fixed_tvpi_dbm_domain_t) =
+    tvpi_split_dbm_domain<number_t, region_subdom_varname_t, DBMParams>;
+using fixed_tvpi_dbm_domain_t =
+    RGN_FUN(ARRAY_FUN(BOOL_NUM(BASE(fixed_tvpi_dbm_domain_t))));
 } // end namespace clam
